9f5aaf90224417e0727a95ec714cba781eb8cfee
Shaun Sharples <shaun.sharples@gmail.com>
2013-07-14 07:26:58 +0200

    playing

--- Octree.hs
+++ Octree.hs
@@ -16,17 +16,12 @@ import Data.Bits
 data Octant     = O0 | O1 | O2 | O3 | O4 | O5 | O6 | O7
                 deriving (Eq, Ord, Enum, Show)
 
-data Octants a  = Octants   { oct0, oct1, oct2, oct3
-                            , oct4, oct5, oct6, oct7 :: !(Octree a)
-                            }
-                deriving (Eq, Show)
-
-data Octree a   = Node  (Octants a)
+data Octree a   = Node  { oct0, oct1, oct2, oct3, oct4, oct5, oct6, oct7 :: !(Octree a) }
                 | Leaf  !a
                 | Empty
                 deriving (Eq, Show)
 
-type Position = (Int, Int, Int)
+type Position   = (Int, Int, Int)
 
 pathToPos :: Int -> Position -> [Octant]
 pathToPos 0 _         = []
@@ -45,44 +40,41 @@ pathToPos h (x, y, z) =
         bit' a b    = if testBit a h' then b else 0 :: Int
         h'          = h - 1
 
-expand :: Octree a -> Octants a
-expand Empty    = Octants Empty Empty Empty Empty Empty Empty Empty Empty
-expand (Leaf v) = Octants (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v)
-expand (Node o) = o
+expand :: Octree a -> Octree a
+expand Empty                            = Node Empty Empty Empty Empty Empty Empty Empty Empty
+expand (Leaf v)                         = Node (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v) (Leaf v)
+expand node                             = node
 
-collapse :: Eq a => Octants a -> Octree a
-collapse (Octants a b c d e f g h) | all (==a) [b,c,d,e,f,g,h] = a
-collapse o                                                     = Node o
+collapse :: Eq a => Octree a -> Octree a
+collapse (Node a b c d e f g h)
+        | all (==a) [b,c,d,e,f,g,h]     = a
+collapse node                           = node
 
 lookup :: Eq a => [Octant] -> Octree a -> Octree a
-lookup (o:os) (Node ocs) =
-    let l o' = lookup os (o' ocs) in
-    case o of
-        O0 -> l oct0
-        O1 -> l oct1
-        O2 -> l oct2
-        O3 -> l oct3
-        O4 -> l oct4
-        O5 -> l oct5
-        O6 -> l oct6
-        O7 -> l oct7
-lookup _      ot       = ot
+lookup (o:os) node@Node{}               = let l o' = lookup os (o' node) in
+                                          case o of O0 -> l oct0
+                                                    O1 -> l oct1
+                                                    O2 -> l oct2
+                                                    O3 -> l oct3
+                                                    O4 -> l oct4
+                                                    O5 -> l oct5
+                                                    O6 -> l oct6
+                                                    O7 -> l oct7
+lookup _      ot                        = ot
 
 modify :: Eq a => (Octree a -> Octree a) -> [Octant] -> Octree a -> Octree a
-modify f []     = f
-modify f (o:os) = collapse . modify' . expand
+modify f []                             = f
+modify f (o:os)                         = collapse . modify' . expand
     where
-        modify' ocs =
-            let m o' = modify f os (o' ocs) in
-            case o of
-                O0 -> ocs { oct0 = m oct0 }
-                O1 -> ocs { oct1 = m oct1 }
-                O2 -> ocs { oct2 = m oct2 }
-                O3 -> ocs { oct3 = m oct3 }
-                O4 -> ocs { oct4 = m oct4 }
-                O5 -> ocs { oct5 = m oct5 }
-                O6 -> ocs { oct6 = m oct6 }
-                O7 -> ocs { oct7 = m oct7 }
+        modify' node                    = let m o' = modify f os (o' node) in
+                                          case o of O0 -> node { oct0 = m oct0 }
+                                                    O1 -> node { oct1 = m oct1 }
+                                                    O2 -> node { oct2 = m oct2 }
+                                                    O3 -> node { oct3 = m oct3 }
+                                                    O4 -> node { oct4 = m oct4 }
+                                                    O5 -> node { oct5 = m oct5 }
+                                                    O6 -> node { oct6 = m oct6 }
+                                                    O7 -> node { oct7 = m oct7 }
 
 empty :: Octree a
 empty = Empty
@@ -95,3 +87,8 @@ insert = set . Leaf
 
 delete :: Eq a => [Octant] -> Octree a -> Octree a
 delete = set Empty
+
+-- toList :: Octree a -> [a]
+-- toList Empty = []
+-- toList (Leaf a) = [a]
+-- toList (Node (Octants a b c d e f g h)) = concatMap toList [a,b,c,d,e,f,g,h]
